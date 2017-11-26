0feec1a02e9f585cbf7ee7ae1738352d36eef8df
Arunwij <arunaswj@gmail.com>
2017-11-26 11:56:48 +0530

    Changed wokload message format

--- src/kademlia/message/ExecuteMessage.java
+++ src/kademlia/message/ExecuteMessage.java
@@ -18,42 +18,35 @@ public class ExecuteMessage implements Message
     private Node origin;
     public static final byte CODE = 0x0B;
 
-    public ExecuteMessage(Node origin)
-    {
+    public ExecuteMessage(Node origin){
         this.origin = origin;
     }
 
-    public ExecuteMessage(DataInputStream in) throws IOException
-    {
+    public ExecuteMessage(DataInputStream in) throws IOException{
         this.fromStream(in);
     }
 
     @Override
-    public final void fromStream(DataInputStream in) throws IOException
-    {
+    public final void fromStream(DataInputStream in) throws IOException{
         this.origin = new Node(in);
     }
 
     @Override
-    public void toStream(DataOutputStream out) throws IOException
-    {
+    public void toStream(DataOutputStream out) throws IOException{
         origin.toStream(out);
     }
 
-    public Node getOrigin()
-    {
+    public Node getOrigin(){
         return this.origin;
     }
 
     @Override
-    public byte code()
-    {
+    public byte code(){
         return CODE;
     }
 
     @Override
-    public String toString()
-    {
-        return "AcknowledgeMessage[origin=" + origin.getNodeId() + "]";
+    public String toString(){
+        return "ExecuteMessage[origin=" + origin.getNodeId() + "]";
     }
 }
