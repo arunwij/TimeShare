a6df0a3111dce4ccba38c8ed030596b295efd65b
Arunwij <arunaswj@gmail.com>
2017-11-28 04:00:45 +0530

    Fine Tuned File sending

---
+++ src/kademlia/file/FileType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kademlia.file;
+
+/**
+ *
+ * @author Artista
+ */
+public class FileType {
+    public static final String SOURCE = "java";
+    public static final String KERNEL = "cu";
+    public static final String DATA = "data";
+}
--- src/timeshare/allocator/Scheduler.java
+++ src/timeshare/allocator/Scheduler.java
@@ -11,14 +11,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Vector;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import kademlia.file.FileSender;
 import kademlia.file.Serializer;
 import kademlia.message.WorkloadManager;
 import kademlia.message.WorkloadMessage;
 import kademlia.node.Node;
-import kademlia.routing.Contact;
 
 /**
  *
@@ -155,15 +152,16 @@ public class Scheduler {
                 for (int l = 0; l < tbu.getTask().files.size(); l++) {
                     System.out.println(tbu.getTask().files.get(l).toString());
                     File file = new File(tbu.getTask().files.get(l).toString());
-
-                    FileSender.send(destiationNode, file, "data/executor/files/");
+                    //Replace File Sender
+                    //.send(destiationNode, file, "data/executor/files/");
 
                 }
 
                 System.out.println("send assest : " + "data/javaFile/" + sim.javaFile.getName());
                 System.out.println("send assest : " + "data/kernel/" + sim.kernel.getName());
-                FileSender.send(destiationNode, sim.javaFile, "data/javaFile/");
-                FileSender.send(destiationNode, sim.kernel, "data/kernel/");
+               // Replace File Sender
+              //  FileSender.send(destiationNode, sim.javaFile, "data/javaFile/");
+              //  FileSender.send(destiationNode, sim.kernel, "data/kernel/");
 
                 // added workload status parameter to workload message. it is a boolean value. True if it is a redundent workload otherwise false;
                 boolean isRedundent = false; // eg
